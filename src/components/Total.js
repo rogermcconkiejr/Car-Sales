1f53ca412867e70bc9834e5b43c6b43ceb78955a
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-11 14:49:59 -0600

    redux store working on Total component

--- src/components/Total.js
+++ src/components/Total.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${props.carPrice + props.addPrice}</h4>
     </div>
   );
 };
@@ -13,7 +13,8 @@ const Total = props => {
 const mapStateToProps = state => {
   console.log('Total mSTP:', state);
   return {
-    
+    carPrice: state.car.price,
+    addPrice: state.additionalPrice
   }
 }
 export default connect(mapStateToProps, {})(Total);
