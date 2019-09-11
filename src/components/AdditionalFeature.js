5b68e5ab23431584d13d53427dcff1920a4d013a
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-09-11 16:24:29 -0600

    fixed additional feature component

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 
 const AdditionalFeature = props => {
@@ -7,16 +6,10 @@ const AdditionalFeature = props => {
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
       <button className="button">Add</button>
-      {props.store.name} (+{props.store.price})
+      {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-const mapStateToProps = state => {
-  console.log('AdditionalFeature mSTP:', state);
-  console.log(state);
-  return {
-    store: state.store
-  }
-}
-export default connect(mapStateToProps, {})(AdditionalFeature);
+
+export default AdditionalFeature;
